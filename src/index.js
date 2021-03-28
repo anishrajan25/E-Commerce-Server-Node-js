b4cf4f6f1984517d7ddd1afbd9cd7f77cd1114bd
Anish Rajan <56666741+anishrajan25@users.noreply.github.com>
2021-03-29 01:03:12 +0530

    added cart data

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const port = 8080;
 const app = express();
 
 app.use(cors());
-// app.use(express.static("public"));
+app.use(express.static("public/images"));
 
 const readAndServeFileData = (filename, res) => {
   fs.readFile(__dirname + filename, (err, data) => {
@@ -16,19 +16,16 @@ const readAndServeFileData = (filename, res) => {
     } else {
       res.set("Content-Type", "application/json");
       res.send(JSON.parse(data));
-      console.log(JSON.parse(data));
     }
   });
 };
 
 app.get("/", (req, res) => {
   readAndServeFileData("/products.json", res);
-  console.log("in root get");
-  // res.end("Hello Welcome!");
 });
 
-app.get("/about", (req, res) => {
-  res.end("About");
+app.get("/cart", (req, res) => {
+  readAndServeFileData("/products.json", res);
 });
 
 app.listen(port, () => {
