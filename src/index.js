84a2aa7fe38ffe8e6fac04e93e762d69d045a2e5
Anish Rajan <56666741+anishrajan25@users.noreply.github.com>
2021-03-28 22:46:20 +0530

    returned products from home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
+const fs = require("fs");
 const express = require("express");
+const cors = require("cors");
 
 const port = 8080;
 
 const app = express();
 
+app.use(cors());
+// app.use(express.static("public"));
+
+const readAndServeFileData = (filename, res) => {
+  fs.readFile(__dirname + filename, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.set("Content-Type", "application/json");
+      res.send(JSON.parse(data));
+      console.log(JSON.parse(data));
+    }
+  });
+};
+
 app.get("/", (req, res) => {
-  res.end("Hello Welcome!");
+  readAndServeFileData("/products.json", res);
+  console.log("in root get");
+  // res.end("Hello Welcome!");
 });
 
 app.get("/about", (req, res) => {
